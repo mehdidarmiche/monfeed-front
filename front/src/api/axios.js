89ff1d55dfffdddde3c34adc7643d5376b851ebc
Hudayfa Koujdal <102149280+hdif004@users.noreply.github.com>
2025-04-03 14:04:54 +0000

    feat: add axios for API requests and create axios instance

---
+++ front/src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:1337/api',
+  timeout: 5000, // Temps d'attente max (optionnel)
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default api;
